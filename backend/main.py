9dac2bf328c97e788c6bb5fd170dd1d31e5f9875
therogerwang <32213225+therogerwang@users.noreply.github.com>
2021-02-06 12:20:15 -0600

    finished recommendation

--- backend/main.py
+++ backend/main.py
@@ -11,9 +11,9 @@ def recommend(business_name):
     
     similar_business_list = get_similar_businesses(business_name)
     
-    vendors = get_vendors_set(similar_business_list)
+    vendors = get_vendors_set(business_name, similar_business_list)
     
-    vendors = filter_vendors_set(business_name,vendors)
+    # vendors = filter_vendors_set(business_name,vendors)
     
 
     
@@ -26,24 +26,49 @@ def get_similar_businesses(business_name):
     
     return db.get_top_x_most_similar_agency(business_name, 5, c)
 
-def get_vendors_set(similar_businesses_list):
+def get_vendors_set(business_name, similar_businesses_list):
     
-    vendors = set([])
+    freq = {}
+    num_vendors_to_output = 10
+    # vendors = set([])
+    already_interacting = db.get_vendors_of_single_agency(business_name, c)
     
     for business_name in similar_businesses_list:
         ven_list = db.get_vendors_of_single_agency(business_name, c)
         for vendor_name in ven_list:
-            vendors.add(vendor_name)
+            
+            #get freq of vendors
+            if vendor_name in already_interacting:
+                continue
+            
+            if vendor_name in freq:
+                freq[vendor_name] += 1
+            else:
+                freq[vendor_name] = 1
+            
+            # vendors.add(vendor_name)
     
-    return vendors
+    #get top 20 most frequent vendors
+    sorted_list = sorted(freq.items(), key=lambda item: item[1])
+    
+    if len(sorted_list) <= num_vendors_to_output:
+        return [x[0] for x in sorted_list]
+        
+    else:
+        return [x[0] for x in sorted_list[-1*num_vendors_to_output:]]
 
 
-def filter_vendors_set(business_name, vendor_set):
-    already_interacting = db.get_vendors_of_single_agency(business_name, c)
-    
-    output = vendor_set.difference(set(already_interacting))
-    
-    return list(output)
+
+# def filter_vendors_set(business_name, vendor_set):
+#     already_interacting = db.get_vendors_of_single_agency(business_name, c)
+#
+#     output = vendor_set.difference(set(already_interacting))
+#
+#     print("vendor set")
+#     print(vendor_set)
+#     print("already interacting")
+#     print(already_interacting)
+#     return list(output)
 
 
 print(recommend("Fire & Emergency Medical Services"))
