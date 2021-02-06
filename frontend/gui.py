46c09deb9cccf49d590f94270763281882d2ee49
Laura Goon <goon.laura@gmail.com>
2021-02-05 22:15:08 -0500

    changed gui to pyqt instead

--- frontend/gui.py
+++ frontend/gui.py
@@ -1,27 +1,22 @@
-from tkinter import *
-import backend.main as bm
+import sys
+
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QLabel
+from PyQt5.QtWidgets import QWidget
 
 class MyWindow:
     def __init__(self, win):
-        self.lbl1 = Label(win, text='Business Name')
-        self.t1 = Entry()
-        self.t2 = Entry()
-        self.btn1 = Button(win, text='Give me some recommendations', command=self.recommend)
-        self.lbl1.place(x=100, y=50)
-        self.t1.place(x=200, y=50)
-        self.btn1.place(x=200, y=75)
-        self.t2.place(x=200, y=100)
-
-    def recommend(self):
-        business_name = self.t1.get()
-        result = bm.recommend(business_name)
-        self.t2.insert(index=END, string=result)
+        helloMsg = QLabel('<h1>Hello World!</h1>', parent=win)
+        helloMsg.move(60, 15)
 
 
-
-
-window = Tk()
+app = QApplication(sys.argv)
+window = QWidget()
 my_win = MyWindow(window)
-window.title('<INSERT TITLE HERE>')
-window.geometry("400x300+10+10")
-window.mainloop()
+
+window.setWindowTitle('PyQt5 App')
+window.setGeometry(100, 100, 1000, 600)
+window.move(60, 15)
+
+window.show()
+sys.exit(app.exec_())
