85eb9e4c814c8f2eaf5a17d7d73a1c3c61ade147
Laura Goon <goon.laura@gmail.com>
2021-02-06 14:12:21 -0500

    gui with new look and logo

--- frontend/gui.py
+++ frontend/gui.py
@@ -1,37 +1,37 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
+import resource_file_rc
 import backend.main as bm
 
 
 class Ui_MainWindow(object):
-
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(954, 652)
+        MainWindow.resize(1016, 729)
         MainWindow.setStyleSheet("background-color:rgb(42, 57, 144)")
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(40, 40, 341, 71))
+        self.label.setGeometry(QtCore.QRect(40, 40, 261, 211))
         self.label.setStyleSheet("color:rgb(255, 255, 255)")
         self.label.setObjectName("label")
         self.label_2 = QtWidgets.QLabel(self.centralwidget)
-        self.label_2.setGeometry(QtCore.QRect(60, 170, 321, 31))
+        self.label_2.setGeometry(QtCore.QRect(90, 330, 321, 31))
         self.label_2.setStyleSheet("color:rgb(255, 255, 255)")
         self.label_2.setObjectName("label_2")
         self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
-        self.lineEdit.setGeometry(QtCore.QRect(60, 210, 281, 24))
+        self.lineEdit.setGeometry(QtCore.QRect(100, 380, 281, 24))
         self.lineEdit.setStyleSheet("color:rgb(255, 255, 255)")
         self.lineEdit.setObjectName("lineEdit")
         self.label_3 = QtWidgets.QLabel(self.centralwidget)
-        self.label_3.setGeometry(QtCore.QRect(530, 110, 341, 31))
+        self.label_3.setGeometry(QtCore.QRect(550, 150, 341, 31))
         self.label_3.setStyleSheet("color:rgb(255, 255, 255)")
         self.label_3.setObjectName("label_3")
         self.label_4 = QtWidgets.QLabel(self.centralwidget)
-        self.label_4.setGeometry(QtCore.QRect(540, 160, 341, 451))
+        self.label_4.setGeometry(QtCore.QRect(560, 210, 341, 451))
         self.label_4.setStyleSheet("color:rgb(255, 255, 255)")
         self.label_4.setObjectName("label_4")
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(60, 250, 121, 32))
+        self.pushButton.setGeometry(QtCore.QRect(100, 420, 121, 32))
         self.pushButton.setStyleSheet("color:rgb(42, 57, 144);\n"
 "background-color: rgb(255, 255, 255);")
         self.pushButton.setObjectName("pushButton")
@@ -45,8 +45,8 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "recommendr"))
-        self.label.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" font-size:28pt; font-weight:600;\">recommendr</span></p></body></html>"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "RecoVend"))
+        self.label.setText(_translate("MainWindow", "<html><head/><body><p><img src=\":/newPrefix/RECOVEND_white.png\"/></p></body></html>"))
         self.label_2.setText(_translate("MainWindow", "<h2>Enter your business name</h2>"))
         self.label_3.setText(_translate("MainWindow", "<h2>Your Top Recommendations</h2>"))
         self.label_3.hide()
